e16f7ec93013b4e43655c3ba7e6933798cdccf3d
Renel Homere <rhomere@gmail.com>
2019-06-06 23:12:06 -0400

    add dbcontext and test library

---
+++ Government.Services/Government.Tests/GovernmentService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Government.Data;
+
+namespace Government.Tests
+{
+    public static class GovernmentService
+    {
+        public static List<Municipal> GetMunicipalities()
+        {
+            using (var context = new CityEntities())
+            {
+                return context.Municipalities.ToList();
+            }
+        }
+
+        public static List<Official> GetOfficials()
+        {
+            using (var context = new CityEntities())
+            {
+                return context.Officials.ToList();
+            }
+        }
+
+        public static List<Address> GetAddresses()
+        {
+            using (var context = new CityEntities())
+            {
+                return context.Addresses.ToList();
+            }
+        }
+
+        public static void DisplayText(string text)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine($"\n{text}\n");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Government.Services/Government.Tests/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Government.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var munis = GovernmentService.GetMunicipalities();
+            var adds = GovernmentService.GetAddresses();
+            var offs = GovernmentService.GetOfficials();
+
+            GovernmentService.DisplayText("MUNICIPALITIES");
+
+            foreach (var item in munis)
+            {
+                Console.WriteLine($"#{item.MunicipalNumber} Name: {item.MunicipalName}");
+            }
+
+            GovernmentService.DisplayText("OFFICIALS");
+
+            foreach (var item in offs)
+            {
+                Console.WriteLine($"Position: {item.Position} Name: {item.FullName}");
+            }
+
+            GovernmentService.DisplayText("ADDRESSES");
+
+            foreach (var item in adds)
+            {
+                Console.WriteLine($"{item.MailingAddressLine1} {item.MailingAddressLine2}, {item.City}, {item.State} {item.Zip}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
